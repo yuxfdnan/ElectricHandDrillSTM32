ca4d0ec766b37aaa708d1e07e66fd61283b40ee6
yuxfdnan <yuxfdnan@126.com>
2018-04-30 11:54:58 +0800

    2018.04.30---V0.01
    刚刚用STM32CubeMX生成的初始化代码

---
+++ Inc/main.h
@@ -0,0 +1,121 @@
+/**
+  ******************************************************************************
+  * File Name          : main.hpp
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* Includes ------------------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define LED_Ctrl_Pin GPIO_PIN_0
+#define LED_Ctrl_GPIO_Port GPIOA
+#define MidAD_Pin GPIO_PIN_1
+#define MidAD_GPIO_Port GPIOA
+#define CurrentAD_Pin GPIO_PIN_2
+#define CurrentAD_GPIO_Port GPIOA
+#define SwtichAD_Pin GPIO_PIN_3
+#define SwtichAD_GPIO_Port GPIOA
+#define MotorAD_A_Pin GPIO_PIN_4
+#define MotorAD_A_GPIO_Port GPIOA
+#define MotorAD_B_Pin GPIO_PIN_5
+#define MotorAD_B_GPIO_Port GPIOA
+#define MotorAD_C_Pin GPIO_PIN_6
+#define MotorAD_C_GPIO_Port GPIOA
+#define Low3_Pin GPIO_PIN_7
+#define Low3_GPIO_Port GPIOA
+#define Low2_Pin GPIO_PIN_0
+#define Low2_GPIO_Port GPIOB
+#define Low1_Pin GPIO_PIN_1
+#define Low1_GPIO_Port GPIOB
+#define High3_Pin GPIO_PIN_8
+#define High3_GPIO_Port GPIOA
+#define High2_Pin GPIO_PIN_9
+#define High2_GPIO_Port GPIOA
+#define High1_Pin GPIO_PIN_10
+#define High1_GPIO_Port GPIOA
+#define Swtich_Pin GPIO_PIN_11
+#define Swtich_GPIO_Port GPIOA
+#define SwtichIn_Pin GPIO_PIN_12
+#define SwtichIn_GPIO_Port GPIOA
+#define Led1_Pin GPIO_PIN_3
+#define Led1_GPIO_Port GPIOB
+#define Led2_Pin GPIO_PIN_4
+#define Led2_GPIO_Port GPIOB
+#define Led3_Pin GPIO_PIN_5
+#define Led3_GPIO_Port GPIOB
+#define Key1_Pin GPIO_PIN_6
+#define Key1_GPIO_Port GPIOB
+#define Key2_Pin GPIO_PIN_7
+#define Key2_GPIO_Port GPIOB
+
+/* ########################## Assert Selection ############################## */
+/**
+  * @brief Uncomment the line below to expanse the "assert_param" macro in the 
+  *        HAL drivers code
+  */
+/* #define USE_FULL_ASSERT    1U */
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+void _Error_Handler(char *, int);
+
+#define Error_Handler() _Error_Handler(__FILE__, __LINE__)
+#ifdef __cplusplus
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MAIN_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
